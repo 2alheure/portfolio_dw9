88221226a6cca29fb571e20ceb85ed3407aa028a
2alheure <jojo77.juv@live.fr>
2023-08-02 16:40:42 +0200

    Projet détails

--- src/Controller/ProjetController.php
+++ src/Controller/ProjetController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Projet;
 use App\Repository\ProjetRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProjetController extends AbstractController {
     #[Route('/projets', name: 'app_projets')]
@@ -13,4 +14,11 @@ class ProjetController extends AbstractController {
         $projets = $projetRepository->findAllForDisplay();
         return $this->render('projet/index.html.twig', compact('projets'));
     }
+    
+    #[Route('/projets/{id}', name: 'app_projet')]
+    public function details(Projet $projet): Response {
+        return $this->render('projet/details.html.twig', compact('projet'));
+    }
+
+
 }
